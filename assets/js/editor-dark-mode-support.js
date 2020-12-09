508c1ce4dd6c5e50cbfbc472dcfbb32eb144484c
Thomas <evers.thomas@gmail.com>
2020-12-09 12:09:44 +0100

    Add files via upload

---
+++ assets/js/editor-dark-mode-support.js
@@ -0,0 +1,44 @@
+/* global wp_tto_end_linkageIsDarkMode, setTimeout */
+
+// Check the color scheme preference and inject the classes if necessary.
+if ( document.body.classList.contains( 'wp_tto_end_linkage-supports-dark-theme' ) ) {
+	wp_tto_end_linkageDarkModeEditorInit();
+}
+
+/**
+ * Once the editor loads, add the dark mode class.
+ *
+ * Wait for the editor to load by periodically checking for an element, then we add the classes.
+ *
+ * @since 1.0.0
+ *
+ * @param {number} attempt Track the number of tries
+ * @return {void}
+ */
+function wp_tto_end_linkageDarkModeEditorInit( attempt ) {
+	var container = document.querySelector( '.block-editor__typewriter' ),
+		maxAttempts = 8;
+
+	// Set the initial attempt if it's undefined.
+	attempt = attempt || 0;
+
+	if ( wp_tto_end_linkageIsDarkMode() ) {
+		if ( null === container ) {
+			// Try again.
+			if ( attempt < maxAttempts ) {
+				setTimeout(
+					function() {
+						wp_tto_end_linkageDarkModeEditorInit( attempt + 1 );
+					},
+					// Double the delay, give the server some time to breathe.
+					25 * Math.pow( 2, attempt )
+				);
+			}
+			return;
+		}
+
+		document.body.classList.add( 'is-dark-theme' );
+		document.documentElement.classList.add( 'is-dark-theme' );
+		container.classList.add( 'is-dark-theme' );
+	}
+}
